64ffb7db50af750d73d1e9d91b1eb023580a4762
KortezR <wowikus99@mail.ru>
2023-09-07 14:40:39 +0300

    Add layouts for main and catalog pages
    They were written before project initializing

--- Spolthy/urls.py
+++ Spolthy/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main import views as main_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', main_views.main_page),
+    path("catalog", main_views.catalog)
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def main_page(request):
+    return render(request, 'index.html')
+
+
+def catalog(request):
+    return render(request, 'catalog.html')
