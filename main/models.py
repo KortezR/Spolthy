d035755d7c0ab8f16c7dc75732519c85eb38cf4e
KortezR <wowikus99@mail.ru>
2023-09-11 20:06:48 +0300

    Manage Exercise model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Exercise(models.Model):
+
+    EXCERCISE_CATEGORIES = [('ch', "грудь"), ('bi', "бицепс"), ('ha', "руки"),
+                            ('tr', "трицепс"), ('le', "ноги"), ('sh', "плечи"),
+                            ('ca', "кардио"),]
+
+    name = models.CharField(unique=True, max_length=100)
+    category = models.CharField(choices=EXCERCISE_CATEGORIES, max_length=2)
+    media = models.ImageField(upload_to='media')
+    description = models.TextField()
+    videolink = models.URLField()
